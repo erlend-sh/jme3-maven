936b5fb0fa788415f9b0ee5802522c97013ca97b
pflanzenmoerder <pflanzenmoerder@gmail.com>
2011-02-17 03:02:01 +0100

    added archetypes for minimal and jbullet projects

---
+++ jme3-bullet-archetype/target/test-classes/projects/basic/project/basic/src/main/java/null/MainGame.java
@@ -0,0 +1,33 @@
+package ${package};
+
+/**
+ * Hello world!
+ *
+ */
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys. */
+public class MainGame extends SimpleApplication {
+
+    public static void main(String[] args) {
+        MainGame app = new MainGame();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(Vector3f.ZERO, 1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/SolidColor.j3md");
+        mat.setColor("m_Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+        rootNode.attachChild(geom);
+    }
+}
